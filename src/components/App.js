0a08422ee014ecdd8c917740d26361e94fb1815c
Jack Pena <penajackc@gmail.com>
2022-02-09 13:15:09 -0700

    completed debugging

--- src/components/App.js
+++ src/components/App.js
@@ -8,22 +8,26 @@ function App() {
   const [orders, setOrders] = useState([]);
 
   function addOrder(order) {
-    setOrders(orders.concat(order));
+    setOrders(orders => [...orders, order]);
   }
 
   const displayOrders = orders.map((order, idx) => {
-    <Order key={idx} {...order} />;
+    return <Order key={idx} order={order} />;
   });
+
+  console.log(displayOrders)
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <Form />
+      <Form addOrder={addOrder}/>
       <div className="ui raised container segment">
         <h1 className="ui block header">All Orders</h1>
-        <div className="ui three cards">{displayOrders}</div>
+        <div className="ui three cards">
+          {displayOrders.length > 0 ? displayOrders : null}
+        </div>
       </div>
     </div>
   );
